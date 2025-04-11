b6e3f2ff2695fb903ad91b7d4d17ddf2cc3fdd05
daniel123mh <pablo.danielmh07@gmail.com>
2025-04-10 19:12:57 -0700

    adsf

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const searchImages = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id='
+    const url = 'https://api.unsplash.com/search/photos/?client_id=k2glIeDajm80v6hiiVqJjHoAHy3C5NWq34ptd73wiUE'
     const res = await axios.get(url,{
         params:{
             query: term
